1778ae9ba5194169a620a89f8994adf1424f4fbb
salvatorebriga <salvo.z2302@gmail.com>
2024-07-06 18:14:03 +0200

    added font awesome script

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,28 +1,25 @@
 <!DOCTYPE html>
 <html lang="en">
 
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title> {{ $title }} </title>
-        @vite('resources/js/app.js')
-    </head>
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="https://kit.fontawesome.com/7711c3f1fc.js" crossorigin="anonymous"></script>
+    <title> {{ $title }} </title>
+    @vite('resources/js/app.js')
+  </head>
 
-    <body>
-        <!-- <img src="{{ Vite::asset('resources/img/duck.jpg') }}" alt="Paperella Laravel Template"> -->
-
-        <header>
-            Header App
-        </header>
+  <body>
+    <!-- <img src="{{ Vite::asset('resources/img/duck.jpg') }}" alt="Paperella Laravel Template"> -->
+    @include('header.header')
 
 
-        <main>
-        </main>
+    <main>
+      @yield('main')
+    </main>
 
-        <footer>
-            Footer App
-        </footer>
-    </body>
+    @include('footer')
+  </body>
 
 </html>
